5200cf3dc379cd6c6dfe02f26ac28f22d5e07682
AtomicAntt <anthonychi36@gmail.com>
2025-02-08 22:55:06 -0800

    Add an on message edit event

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import discord
-from discord.ext import commands
+
+# ENV FILE (to hide token) ==================================
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 bot_token = os.getenv("DISCORD_BOT_TOKEN")
+# ENV FILE (to hide token) ==================================
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-# client = discord.Client(intents=intents)
-client = commands.Bot(command_prefix='$', intents=intents)
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -24,17 +25,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith(';hello'):
         await message.channel.send('Hello!')
-    
-    await client.process_commands(message)
 
 @client.event
 async def on_reaction_add(reaction, user):
     await reaction.message.channel.send("Someone just added a reaction to a message!")
 
-@client.command()
-async def add(ctx, arg1: int, arg2: int):
-    await ctx.send(str(arg1 + arg2))
+@client.event
+async def on_message_edit(before, after):
+    await before.channel.send(
+        f'{before.author} just editted a message!\n'
+        f'Before: {before.content}\n'
+        f'After: {after.content}'
+    )
 
 client.run(bot_token)
